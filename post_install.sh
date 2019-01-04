345d372f2e2855991229fe237033a3d9fb76fbc8
Brandon Schneider <brandon@ixsystems.com>
2019-01-04 14:29:45 -0600

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -6,5 +6,6 @@ sysrc -f /etc/rc.conf rslsync_enable="YES"
 # Start the service
 service rslsync start 2>/dev/null
 
+echo "Running"
 echo "rslsync now installed" > /root/PLUGIN_INFO
 echo "foo" >> /root/PLUGIN_INFO
